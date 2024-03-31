af43c5039103fc94b87c40d8d6c913ffe42096b6
Miranda <fmiranda@bankly.com.br>
2024-03-31 16:45:26 -0300

    feat add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app/app.module';
+import { AppModule } from '@/modules/app/app.module';
+import { SwaggerStartup } from '@/infra/startup/swagger.startup';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  SwaggerStartup.init(app);
   await app.listen(80);
 }
 bootstrap();
